7771824c2cb65c532099584798ec1ff72fc55814
Daniel-Kevlar <teokaijie@gmail.com>
2016-02-23 11:58:55 +0800

    Attempted Fix for Enemy Class

--- MyGraphics/Source/Enemy.cpp
+++ MyGraphics/Source/Enemy.cpp
@@ -1,18 +1,28 @@
 #include "Enemy.h"
 
-
 Enemy::Enemy()
 {
+	health = 10;
+	damage = 1;
 }
 
 
 Enemy::~Enemy()
 {
 }
+/*
+void Enemy::addEnemy(Enemy newGuy)
+{
+	Enemy enemyArray[10];
+	for (int i; i < 10; i++)
+	{
+		enemyArray[i] = newGuy;
+	}
+}*/
 
 int Enemy::getHealth(void)
 {
-	//Add code
+	//add stuff
 
 	return 10;
 }
@@ -46,23 +56,49 @@ bool Enemy::isDead(Enemy testSubject)
 	}
 }
 
-int Enemy::getDistance(Enemy temp, Camera2 playerPos)
-{
-	distToPlayer = playerPos.position.Dot(temp.position);
-
-	return distToPlayer;
-}
-
-void Enemy::setDistance(int dist)
-{
-	distToPlayer = dist;
-}
-
 void Enemy::Update(double dt)
 {
-	Enemy ayylmao;
-	Character player;
-	if (distToPlayer <= 20)
+	Vector3 radius = (20, 20, 20);
+	int randomized = rand() % 10 + 1;
+	playerPos = SharedData::GetInstance()->PlayerPosition;
+
+	distToPlayer.x = playerPos.x - position.x;
+	distToPlayer.y = playerPos.y - position.y;
+	distToPlayer.z = playerPos.z - position.z;
+
+	distToPlayer.Normalize();
+
+	//
+	if (distToPlayer.x < radius.x && distToPlayer.y < radius.y && distToPlayer.z < radius.z)
+	{
+		distToPlayer.x - (float)(10 * dt);
+		distToPlayer.y - (float)(10 * dt);
+		distToPlayer.z - (float)(10 * dt);
+	}
+	
+
+
+
+	/*switch (eState)
+	{
+	case ENEMY_IDLE:
+		
+		break;
+
+	case ENEMY_CHASE:
+		distToPlayer--;
+		break;
+
+	case ENEMY_ATTACK:
+		player.recieveDamage(1);
+		break;
+
+	case ENEMY_DIE:
+		break;
+
+	}*/
+
+	/*if (distToPlayer <= 20)
 	{
 		eState = ENEMY_CHASE;
 		distToPlayer--;
@@ -81,5 +117,8 @@ void Enemy::Update(double dt)
 	else
 	{
 		eState = ENEMY_IDLE;
-	}
+	}*/
+
+
 }
+
